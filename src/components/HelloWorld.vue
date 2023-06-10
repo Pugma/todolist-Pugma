e9690485202e8ca987ce26cef3807ad94a9199a9
Pugma <132571355+Pugma@users.noreply.github.com>
2023-06-10 18:26:51 +0900

    とりあえずここまで作った！！！

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,4 +1,8 @@
 <script setup lang="ts">
+
+import ClickCounter from './ClickCounter.vue'
+import ItemList from './ItemList.vue'
+
 defineProps<{
   msg: string
 }>()
@@ -6,12 +10,9 @@ defineProps<{
 
 <template>
   <div class="greetings">
-    <h1 class="green">{{ msg }}</h1>
-    <h3>
-      You’ve successfully created a project with
-      <a href="https://vitejs.dev/" target="_blank" rel="noopener">Vite</a> +
-      <a href="https://vuejs.org/" target="_blank" rel="noopener">Vue 3</a>.
-    </h3>
+    <h1>{{ msg }}</h1>
+    <ClickCounter />
+    <ItemList />
   </div>
 </template>
 
